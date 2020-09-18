5007611a677813340c2b9dc02deccc6a8bac8c80
Jun <70848796+hvgfhfk@users.noreply.github.com>
2020-09-18 19:02:58 +0900

    token

--- Discord Chat Log/rcall.cs
+++ Discord Chat Log/rcall.cs
@@ -44,8 +44,8 @@ namespace Raidcall
         {
             await _client.SetStatusAsync(UserStatus.DoNotDisturb); // 봇의 상태 메세지 변경
             await _client.SetGameAsync("수성준비 ");
-          //  await _client.LoginAsync(TokenType.Bot, "NzUxMzU5OTg0MzMxMjU5OTU1.X1H8gw.IVE0uPvFNcPthQuQ0Qx61C1SfSA"); // CALL 봇
-            await _client.LoginAsync(TokenType.Bot, "NzU2NDM5ODc3OTYwMzM1Mzcw.X2R3iA.IHu_1-gDeN7gja27RhyYOcBKkA8");
+            await _client.LoginAsync(TokenType.Bot, "NzUxMzU5OTg0MzMxMjU5OTU1.X1H8gw.IVE0uPvFNcPthQuQ0Qx61C1SfSA"); // CALL 봇
+          //  await _client.LoginAsync(TokenType.Bot, "NzU2NDM5ODc3OTYwMzM1Mzcw.X2R3iA.IHu_1-gDeN7gja27RhyYOcBKkA8");
             await _client.StartAsync();
             await Task.Delay(-1);
         }
@@ -66,27 +66,26 @@ namespace Raidcall
 
         public async Task MessageReceivedAsync(SocketMessage message)
         {
-            var Text_Channel__01chl = message.Channel.Id == 682591375036121258;
-            var Text_Channel__12chl = message.Channel.Id == 692450471813578814;
-            var Text_Channel__23chl = message.Channel.Id == 747345150782603286;
-            var Text_Channel__34chl = message.Channel.Id == 756440639716982804;
+            var Text_Channel__chat_chl = message.Channel.Id == 682591375036121258;
+            var Text_Channel__mainchat_chl = message.Channel.Id == 692450471813578814;
+            var Text_Channel__startchat_chl = message.Channel.Id == 747345150782603286;
 
-            var Text_Channel__Among = message.Channel.Id == 756440639716982804;
+            var Text_Channel__Among = message.Channel.Id == 753241021692903435;
 
 
-             if (Text_Channel__01chl || Text_Channel__12chl || Text_Channel__23chl)
+             if (Text_Channel__chat_chl || Text_Channel__mainchat_chl || Text_Channel__startchat_chl)
              {
-                 if(message.Content == "@ㄹㅇㄷ")
-                 {
-                     await message.DeleteAsync();
-                 }
-             }
+                for (int i = 0; i < 5; i++)
+                {
+                    await message.Channel.SendMessageAsync("@everyone 레이드!!");
+                }
+            }
              else if(message.Content == "@ㄹㅇㄷ")
              {
-                 for (int i = 0; i < 5; i++)
-                 {
-                     await message.Channel.SendMessageAsync("@everyone 레이드!!");
-                 }
+                if (message.Content == "@ㄹㅇㄷ")
+                {
+                    await message.DeleteAsync();
+                }
              } 
 
             if (Text_Channel__Among)
